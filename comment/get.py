7f5d53638d16b75e4017c755887f6f82563ddda3
KipCrossing <kip.crossing@gmail.com>
2020-06-01 08:54:05 +1000

    Make GET comments more general

--- comment/get.py
+++ comment/get.py
@@ -10,14 +10,9 @@ collection = db[mongosettings[COMMENTSCOLLECTION]]
 
 
 def get(event, context):
-    command_items = event['pathParameters']['command'].split("-")
-
-
-    if len(command_items) == 1:
-        topic = command_items[0]
-        num_comments = collection.find({'topic': topic}).count()
-
-        # comments = list(collection.find({'topic':topic}))
+    command_items = event['pathParameters']['command']
+    try:
+        topic = command_items
         comments = list(collection.find({'topic':topic}))
         comments.reverse()
 
@@ -25,32 +20,11 @@ def get(event, context):
             "statusCode": 200,
             "body": json.dumps(comments)
         }
-    else: 
+    except Exception as e: 
         response = {
             "statusCode": 400,
-            "body": str("Bad command"),
+            "body": str(e),
         }
-        
+
     return response
-    # elif len(command_items) == 3:
-    #     topic = command_items[0]
-    #     num_comments = collection.find({'topic': topic}).count()
-    #     min_range = int(command_items[1])
-    #     max_range = int(command_items[2])
-    #     if min_range < 1:
-    #         min_range = 1
-    #     print(min_range)
-    #     comments = list(collection.find({"number": {"$lt": max_range + 1, "$gt":min_range - 1}, 'topic':topic}))
-    #     print(topic,str(min_range),str(max_range))
-    #     body = {
-    #         "current": topic + "-" + str(min_range) + "-" + str(max_range),
-    #         "next": "r1234-20-30",
-    #         "comments": comments,
-    #     }
-    # ballot = collection.find_one({"_id": ballotspec_id})
-
-    # create a response
-
-
-    # return response
     
